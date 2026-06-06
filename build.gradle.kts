85bc279ce0e0e31d95be6a7593788ee5868c6029
Cherryh4ck <tomas1234567891016@gmail.com>
2026-06-06 01:26:40 -0300

    bump to 1.1 (hopefully no bugs)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.Cherryh4ck"
-version = "1.0b12a"
+version = "1.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/io/github/Cherryh4ck/toms3Core/Toms3Core.kt
+++ src/main/kotlin/io/github/Cherryh4ck/toms3Core/Toms3Core.kt
@@ -294,14 +294,14 @@ class Toms3Core : JavaPlugin() {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         val mensaje : Component
         if(args.isEmpty()){
-            mensaje = minimessage.deserialize("<gold>$prefix version ${this.pluginMeta.version} (build 12a)</gold>")
+            mensaje = minimessage.deserialize("<gold>$prefix version ${this.pluginMeta.version}</gold>")
             sender.sendMessage(mensaje)
             return true
         }
 
         when (args[0].lowercase()) {
             "help" -> {
-                mensaje = minimessage.deserialize("$prefix <gold>Available commands:<newline>- <gray>/tmcore</gray><newline>- <gray>/tmcore get_player_by_uuid</gray><newline>- <gray>/tmcore get_uuid_by_player</gray><newline>- <gray>/joindate</gray><newline>- <gray>/playtime</gray><newline>- <gray>/lastseen</gray><newline>- <gray>/dupe</gray><newline>- <gray>/vote</gray><newline>- <gray>/seed</gray></gold>")
+                mensaje = minimessage.deserialize("$prefix <gold>Available commands:<newline>- <gray>/tmcore</gray><newline>- <gray>/tmcore get_player_by_uuid</gray><newline>- <gray>/tmcore get_uuid_by_player</gray><newline>- <gray>/joindate</gray><newline>- <gray>/playtime</gray><newline>- <gray>/lastseen</gray><newline>- <gray>/dupe</gray><newline>- <gray>/vote</gray><newline>- <gray>/seed</gray><newline>- <gray>/toggleannouncements</gray></gold>")
                 sender.sendMessage(mensaje)
             }
             "reload" -> {
